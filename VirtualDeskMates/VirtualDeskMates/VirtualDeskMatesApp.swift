d6eaab751220308b164eadd4ebe3cbbad5bf92b3
Jeremy Cummings <jmilescummings@gmail.com>
2025-01-31 21:57:46 -0700

    Initial Xcode project setup

---
+++ VirtualDeskMates/VirtualDeskMates/VirtualDeskMatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualDeskMatesApp.swift
+//  VirtualDeskMates
+//
+//  Created by JMC on 1/31/25.
+//
+
+import SwiftUI
+
+@main
+struct VirtualDeskMatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
